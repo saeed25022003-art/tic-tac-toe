d256399e0127d36fcae43f7e2f2f39f215671172
saeed <saeed25022003@gmail.com>
2026-04-14 11:52:46 +0530

    sec

--- app.js
+++ app.js
@@ -1,90 +1,116 @@
 let boxes = document.querySelectorAll(".box");
-let reset = document.querySelector("#reset");
-let newgame = document.querySelector("#new-btn");
-let msgContainer = document.querySelector(".msg-container");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector("#msg-container");
 let msg = document.querySelector("#msg");
 let scoreO = document.querySelector("#scoreO");
 let scoreX = document.querySelector("#scoreX");
-let turnO= true;//playerX,playerO
-let gameOver = false;
 
-const winPatterns =[
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8],
+let turnO = true; // true = O, false = X
+let count = 0; // To track Draw
+
+const winPatterns = [
+    [0, 1, 2], [0, 3, 6], [0, 4, 8],
+    [1, 4, 7], [2, 5, 8], [2, 4, 6],
+    [3, 4, 5], [6, 7, 8],
 ];
-function resetPage() {
-    localStorage.setItem("keyO",0);
-    localStorage.setItem("keyX",0);
-    location.reload();
-}
-function startPage() {
-    location.reload();
-}
-boxes.forEach((box) => 
-{
-box.addEventListener("click",() =>{
-    if(gameOver){
-    location.reload();
-    return;
-   }
-   if(turnO){
-   box.innerText="O";
-   turnO=false;
-   }else{
-    box.innerText="X";
-    turnO=true;
-   }
-   box.disabled=true;
-   checkWinner();
-});
 
-});
-const showWinner = (winner) => {
-    let currentScore = Number(localStorage.getItem(`key${winner}`)) || 0;
-    let newScore = currentScore + 1;
-    
-    localStorage.setItem(`key${winner}`, newScore);
-    
-    if (winner === 'X') {
-        scoreX.innerText = `X: ${newScore}`;
-    } else {
-        scoreO.innerText = `O: ${newScore}`;
-    }
-
-    msg.innerText = `${winner} Wins`;
-    msgContainer.classList.remove("hide");
-    disableBoxes(); 
+// Initialize scores from LocalStorage
+window.onload = () => {
+    scoreX.innerText = `X: ${localStorage.getItem("keyX") || 0}`;
+    scoreO.innerText = `O: ${localStorage.getItem("keyO") || 0}`;
 };
+
+const resetGame = () => {
+    turnO = true;
+    count = 0;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+const resetScores = () => {
+    localStorage.setItem("keyO", 0);
+    localStorage.setItem("keyX", 0);
+    scoreO.innerText = "O: 0";
+    scoreX.innerText = "X: 0";
+    resetGame();
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turnO) {
+            box.innerText = "O";
+            box.style.color = "#b0413e";
+            turnO = false;
+        } else {
+            box.innerText = "X";
+            box.style.color = "blue";
+            turnO = true;
+        }
+        box.disabled = true;
+        count++;
+
+        let isWinner = checkWinner();
+
+        if (count === 9 && !isWinner) {
+            gameDraw();
+        }
+    });
+});
+
+const gameDraw = () => {
+    msg.innerText = `Game was a Draw.`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
 const disableBoxes = () => {
     for (let box of boxes) {
         box.disabled = true;
     }
 };
 
-window.onload = () => {
-    scoreX.innerText = `X: ${localStorage.getItem("keyX") || 0}`;
-    scoreO.innerText = `O: ${localStorage.getItem("keyO") || 0}`;
-};
-
-
-const checkWinner = () => {
-    for(let pattern of winPatterns){
-        let pos1val=boxes[pattern[0]].innerText;
-        let pos2val=boxes[pattern[1]].innerText;
-        let pos3val=boxes[pattern[2]].innerText;
-        if(pos1val != "" && pos2val != "" && pos3val != ""){
-            if(pos1val === pos2val && pos2val === pos3val){
-                showWinner(pos1val);
-                gameOver=true;
-                return;
-            }
-
-        }
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
     }
 };
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+    updateScore(winner);
+};
+
+const updateScore = (winner) => {
+    let currentScore = Number(localStorage.getItem(`key${winner}`)) || 0;
+    let newScore = currentScore + 1;
+    localStorage.setItem(`key${winner}`, newScore);
+    
+    if (winner === "X") {
+        scoreX.innerText = `X: ${newScore}`;
+    } else {
+        scoreO.innerText = `O: ${newScore}`;
+    }
+};
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val);
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetScores);
